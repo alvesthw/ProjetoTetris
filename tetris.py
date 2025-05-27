b0a4c55fc18d1648ca24a27b268a418d19859867
alvesthw <matbmac@gmail.com>
2025-05-27 20:54:26 -0300

    mudanças

--- tetris.py
+++ tetris.py
@@ -5,10 +5,10 @@ import random
 # Constantes
 LINHAS, COLUNAS = 20, 10
 TAMANHO_BLOCO = 30
-LARGURA_JANELA, ALTURA_JANELA = COLUNAS * TAMANHO_BLOCO, LINHAS * TAMANHO_BLOCO
+LARGURA_JANELA, ALTURA_JANELA = COLUNAS * TAMANHO_BLOCO * 2, LINHAS * TAMANHO_BLOCO  # duas áreas lado a lado
 
-# Peças do Tetris (formatos)
-PECAS_TETRIS = {
+# Peças do Tetris
+PEÇAS_TETRIS = {
     'I': [[1, 1, 1, 1]],
     'O': [[1, 1],
           [1, 1]],
@@ -24,124 +24,157 @@ PECAS_TETRIS = {
           [1, 1, 1]]
 }
 
-# Estado do jogo
-grade = [[0] * COLUNAS for _ in range(LINHAS)]
-peca_atual = None
-posicao_peca = [0, 3]  # linha, coluna
+def rotacionar(formato):
+    return [list(linha) for linha in zip(*formato[::-1])]
 
-def desenhar_bloco(x, y, cor):
-    glColor3fv(cor)
-    glBegin(GL_QUADS)
-    glVertex2f(x, y)
-    glVertex2f(x + TAMANHO_BLOCO, y)
-    glVertex2f(x + TAMANHO_BLOCO, y + TAMANHO_BLOCO)
-    glVertex2f(x, y + TAMANHO_BLOCO)
-    glEnd()
+class Jogador:
+    def __init__(self, deslocamento_x, teclas):
+        self.deslocamento_x = deslocamento_x  # deslocamento horizontal na tela para desenhar
+        self.teclas = teclas  # dicionário com as teclas (esq, dir, baixo, rotacionar)
+        self.grade = [[0] * COLUNAS for _ in range(LINHAS)]
+        self.peca_atual = None
+        self.pos_peca = [0, 3]
+        self.gerar_peca()
 
-def desenhar_grade():
-    for linha in range(LINHAS):
-        for coluna in range(COLUNAS):
-            if grade[linha][coluna]:
-                desenhar_bloco(coluna * TAMANHO_BLOCO, (LINHAS - linha - 1) * TAMANHO_BLOCO, (0.8, 0.2, 0.2))
+    def gerar_peca(self):
+        self.peca_atual = {'tipo': random.choice(list(PEÇAS_TETRIS.keys())), 'formato': None}
+        self.peca_atual['formato'] = PEÇAS_TETRIS[self.peca_atual['tipo']]
+        self.pos_peca = [0, 3]
 
-def desenhar_peca():
-    if not peca_atual:
-        return
-    forma = peca_atual['forma']
-    for i, linha in enumerate(forma):
-        for j, val in enumerate(linha):
-            if val:
-                desenhar_bloco((posicao_peca[1] + j) * TAMANHO_BLOCO, (LINHAS - posicao_peca[0] - i - 1) * TAMANHO_BLOCO, (0.2, 0.8, 0.2))
+    def peca_cabe(self, pos, formato):
+        for i, linha in enumerate(formato):
+            for j, val in enumerate(linha):
+                if val:
+                    l, c = pos[0] + i, pos[1] + j
+                    if l >= LINHAS or c < 0 or c >= COLUNAS or (l >= 0 and self.grade[l][c]):
+                        return False
+        return True
 
-def gerar_nova_peca():
-    global peca_atual, posicao_peca
-    tipo = random.choice(list(PECAS_TETRIS.keys()))
-    forma = PECAS_TETRIS[tipo]
-    peca_atual = {'tipo': tipo, 'forma': forma}
-    posicao_peca[:] = [0, 3]
+    def fixar_peca(self):
+        formato = self.peca_atual['formato']
+        for i, linha in enumerate(formato):
+            for j, val in enumerate(linha):
+                if val:
+                    l, c = self.pos_peca[0] + i, self.pos_peca[1] + j
+                    self.grade[l][c] = 1
+        self.remover_linhas()
+        self.gerar_peca()
 
-def rotacionar(matriz):
-    return [list(linha) for linha in zip(*matriz[::-1])]
+    def remover_linhas(self):
+        self.grade = [linha for linha in self.grade if not all(linha)]
+        while len(self.grade) < LINHAS:
+            self.grade.insert(0, [0] * COLUNAS)
 
-def peca_cabe(nova_pos, nova_forma):
-    for i, linha in enumerate(nova_forma):
-        for j, val in enumerate(linha):
-            if val:
-                r, c = nova_pos[0] + i, nova_pos[1] + j
-                if r >= LINHAS or c < 0 or c >= COLUNAS or (r >= 0 and grade[r][c]):
-                    return False
-    return True
+    def atualizar(self):
+        nova_pos = [self.pos_peca[0] + 1, self.pos_peca[1]]
+        if self.peca_cabe(nova_pos, self.peca_atual['formato']):
+            self.pos_peca[0] += 1
+        else:
+            self.fixar_peca()
 
-def fixar_peca():
-    forma = peca_atual['forma']
-    for i, linha in enumerate(forma):
-        for j, val in enumerate(linha):
-            if val:
-                r, c = posicao_peca[0] + i, posicao_peca[1] + j
-                grade[r][c] = 1
-    limpar_linhas()
-    gerar_nova_peca()
+    def mover_esquerda(self):
+        nova_pos = [self.pos_peca[0], self.pos_peca[1] - 1]
+        if self.peca_cabe(nova_pos, self.peca_atual['formato']):
+            self.pos_peca[1] -= 1
 
-def limpar_linhas():
-    global grade
-    grade = [linha for linha in grade if not all(linha)]
-    while len(grade) < LINHAS:
-        grade.insert(0, [0] * COLUNAS)
+    def mover_direita(self):
+        nova_pos = [self.pos_peca[0], self.pos_peca[1] + 1]
+        if self.peca_cabe(nova_pos, self.peca_atual['formato']):
+            self.pos_peca[1] += 1
 
-def atualizar():
-    nova_pos = [posicao_peca[0] + 1, posicao_peca[1]]
-    if peca_cabe(nova_pos, peca_atual['forma']):
-        posicao_peca[0] += 1
-    else:
-        fixar_peca()
+    def mover_baixo(self):
+        self.atualizar()
 
-def tratar_teclas(janela, tecla, scancode, acao, mods):
+    def rotacionar_peca(self):
+        novo_formato = rotacionar(self.peca_atual['formato'])
+        if self.peca_cabe(self.pos_peca, novo_formato):
+            self.peca_atual['formato'] = novo_formato
+
+    def desenhar_bloco(self, x, y, cor):
+        glColor3fv(cor)
+        glBegin(GL_QUADS)
+        glVertex2f(x, y)
+        glVertex2f(x + TAMANHO_BLOCO, y)
+        glVertex2f(x + TAMANHO_BLOCO, y + TAMANHO_BLOCO)
+        glVertex2f(x, y + TAMANHO_BLOCO)
+        glEnd()
+
+    def desenhar_grade(self):
+        for l in range(LINHAS):
+            for c in range(COLUNAS):
+                if self.grade[l][c]:
+                    self.desenhar_bloco(self.deslocamento_x + c * TAMANHO_BLOCO, (LINHAS - l - 1) * TAMANHO_BLOCO, (0.8, 0.2, 0.2))
+
+    def desenhar_peca(self):
+        formato = self.peca_atual['formato']
+        for i, linha in enumerate(formato):
+            for j, val in enumerate(linha):
+                if val:
+                    self.desenhar_bloco(self.deslocamento_x + (self.pos_peca[1] + j) * TAMANHO_BLOCO, (LINHAS - self.pos_peca[0] - i - 1) * TAMANHO_BLOCO, (0.2, 0.8, 0.2))
+
+# Configuração dos jogadores
+# Jogador 1 usa setas: esquerda, direita, baixo, cima (rotacionar)
+teclas_jogador1 = {
+    'esquerda': glfw.KEY_LEFT,
+    'direita': glfw.KEY_RIGHT,
+    'baixo': glfw.KEY_DOWN,
+    'rotacionar': glfw.KEY_UP
+}
+# Jogador 2 usa WASD: A, D, S, W (rotacionar)
+teclas_jogador2 = {
+    'esquerda': glfw.KEY_A,
+    'direita': glfw.KEY_D,
+    'baixo': glfw.KEY_S,
+    'rotacionar': glfw.KEY_W
+}
+
+jogadores = [
+    Jogador(0, teclas_jogador1),
+    Jogador(COLUNAS * TAMANHO_BLOCO, teclas_jogador2)
+]
+
+def tecla_callback(janela, tecla, scancode, acao, mods):
     if acao != glfw.PRESS:
         return
-
-    if tecla == glfw.KEY_LEFT:
-        nova_pos = [posicao_peca[0], posicao_peca[1] - 1]
-        if peca_cabe(nova_pos, peca_atual['forma']):
-            posicao_peca[1] -= 1
-    elif tecla == glfw.KEY_RIGHT:
-        nova_pos = [posicao_peca[0], posicao_peca[1] + 1]
-        if peca_cabe(nova_pos, peca_atual['forma']):
-            posicao_peca[1] += 1
-    elif tecla == glfw.KEY_DOWN:
-        atualizar()
-    elif tecla == glfw.KEY_UP:
-        nova_forma = rotacionar(peca_atual['forma'])
-        if peca_cabe(posicao_peca, nova_forma):
-            peca_atual['forma'] = nova_forma
+    for jogador in jogadores:
+        if tecla == jogador.teclas['esquerda']:
+            jogador.mover_esquerda()
+        elif tecla == jogador.teclas['direita']:
+            jogador.mover_direita()
+        elif tecla == jogador.teclas['baixo']:
+            jogador.mover_baixo()
+        elif tecla == jogador.teclas['rotacionar']:
+            jogador.rotacionar_peca()
 
 def principal():
     if not glfw.init():
         return
-    janela = glfw.create_window(LARGURA_JANELA, ALTURA_JANELA, "Tetris 2D", None, None)
+    janela = glfw.create_window(LARGURA_JANELA, ALTURA_JANELA, "Tetris Multijogador Local", None, None)
     if not janela:
         glfw.terminate()
         return
     glfw.make_context_current(janela)
-    glfw.set_key_callback(janela, tratar_teclas)
+    glfw.set_key_callback(janela, tecla_callback)
 
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
     glOrtho(0, LARGURA_JANELA, 0, ALTURA_JANELA, -1, 1)
     glMatrixMode(GL_MODELVIEW)
 
-    gerar_nova_peca()
     tempo_anterior = glfw.get_time()
     velocidade = 0.5
 
     while not glfw.window_should_close(janela):
         agora = glfw.get_time()
         if agora - tempo_anterior > velocidade:
-            atualizar()
+            for jogador in jogadores:
+                jogador.atualizar()
             tempo_anterior = agora
 
         glClear(GL_COLOR_BUFFER_BIT)
-        desenhar_grade()
-        desenhar_peca()
+        for jogador in jogadores:
+            jogador.desenhar_grade()
+            jogador.desenhar_peca()
         glfw.swap_buffers(janela)
         glfw.poll_events()
 
